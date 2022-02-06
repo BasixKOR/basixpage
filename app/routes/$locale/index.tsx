6f34abd2238408af21d36a96656912195fb8dc36
Basix <basix@basix.tech>
2022-02-06 21:49:22 +0900

    Add dedicated homepage
    Y'know what, it's already better than the Prismic one.

--- app/routes/$locale/index.tsx
+++ app/routes/$locale/index.tsx
@@ -1,20 +1,74 @@
-import { LoaderFunction, redirect, useLoaderData } from "remix";
+import { useQuerySubscription } from "react-datocms";
+import { LoaderFunction, useLoaderData, useOutletContext } from "remix";
+import invariant from "tiny-invariant";
+import ArticlesList from "~/components/ArticlesList";
+import { StructuredText } from "~/components/dato";
+import { HomepageQuery } from "~/graphql/generated";
+import { OutletData } from "~/root";
+import { datoQuerySubscription, gql, QueryListenerOptions } from "~/utils/dato";
+import { fragment as articlesListFragment } from "~/components/ArticlesList";
 
-export const loader: LoaderFunction = ({ params }) => {
-  return redirect('posts');
+export const loader: LoaderFunction = ({ params, request }) => {
+  return datoQuerySubscription({
+    request,
+    query: gql`
+      query Homepage($locale: SiteLocale) {
+        homePage(locale: $locale) {
+          title
+          note {
+            value
+            links {
+              __typename
+              ... on ArticleRecord {
+                id
+                slug
+                title
+              }
+            }
+          }
+          content {
+            __typename
+            ... on ArticlesListRecord {
+              articles {
+                ...articlesList
+              }
+            }
+          }
+        }
+      }
+      ${articlesListFragment}
+    `,
+    variables: {
+      locale: params.locale,
+    },
+  });
 };
 
-// export default function Index() {
-// 	const data = useLoaderData<Home>();
+export default function Index() {
+  const query = useLoaderData<QueryListenerOptions<HomepageQuery>>();
+  const { data } = useQuerySubscription(query);
+  const { locale } = useOutletContext<OutletData>();
 
-//   return (
-//     <div className="container">
-//       <article>
-// 				<header>
-// 					<PrismicRichText field={data.data.title} />
-// 				</header>
-// 				<PrismicRichText field={data.data.content} />
-//       </article>
-//     </div>
-//   );
-// }
+  invariant(data, "data is undefined");
+
+  return (
+    <div className="container">
+      <article>
+        {data.homePage?.title && data.homePage.note && (
+          <header>
+            <h1>{data.homePage.title}</h1>
+            <StructuredText data={data.homePage.note} locale={locale} />
+          </header>
+        )}
+        {data.homePage?.content.map((item) => {
+          switch (item.__typename) {
+            case "ArticlesListRecord":
+              return <ArticlesList data={item.articles} locale={locale} />;
+            default:
+              return <span>{item.__typename}</span>;
+          }
+        })}
+      </article>
+    </div>
+  );
+}
