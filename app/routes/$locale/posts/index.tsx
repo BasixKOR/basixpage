e9959827df7a88306cabbf48b621eb4dfb2a8769
Basix <basix@basix.tech>
2022-02-06 20:28:22 +0900

    Move ArticlesList to a component
    I'm making it into a block.

--- app/routes/$locale/posts/index.tsx
+++ app/routes/$locale/posts/index.tsx
@@ -1,61 +1,42 @@
-import { formatRelative, parseISO } from "date-fns";
 import { useQuerySubscription } from "react-datocms";
-import { Calendar } from "react-feather";
-import { Link, LoaderFunction, useLoaderData, useOutletContext } from "remix";
+import { LoaderFunction, useLoaderData, useOutletContext } from "remix";
+import invariant from "tiny-invariant";
+import ArticlesList, { fragment as articlesListFragment } from "~/components/ArticlesList";
 import { GetPostsQuery } from "~/graphql/generated";
 import { OutletData } from "~/root";
 import { datoQuerySubscription, gql, QueryListenerOptions } from "~/utils/dato";
-import { getDateFnsLocale } from "~/utils/i18n";
 
 export const loader = async ({
   params,
   request,
 }: Parameters<LoaderFunction>[0]) => {
-  const a = await datoQuerySubscription({
+  return datoQuerySubscription({
     request,
     query: gql`
       query getPosts($locale: SiteLocale) {
         allArticles(locale: $locale) {
-          createdAt
-          id
-          slug
-          title
-          description
+          ...articlesList
         }
       }
+      ${articlesListFragment}
     `,
     variables: {
       locale: params.locale,
     },
   });
-  debugger;
-  console.log("from loader:", a);
-  return a;
 };
 
 export default function Posts() {
-  debugger
+  debugger;
   const query = useLoaderData<QueryListenerOptions<GetPostsQuery>>();
   const { data } = useQuerySubscription(query);
   const { locale } = useOutletContext<OutletData>();
 
+  invariant(data, "data is undefined");
+
   return (
     <div className="container">
-      {data!.allArticles.map((post) => (
-        <article key={post.slug}>
-          <Link to={`/${locale}/${post.slug}`}>
-            <h1>{post.title}</h1>
-          </Link>
-          <span>
-            <Calendar />{" "}
-            {post.createdAt &&
-              formatRelative(parseISO(post.createdAt), new Date(), {
-                locale: getDateFnsLocale(locale),
-              })}
-          </span>
-          <p>{post.description}</p>
-        </article>
-      ))}
+      <ArticlesList data={data.allArticles} locale={locale} />
     </div>
   );
 }
