d8d32628e4962e1028c2b0e4bd6be288450b79a7
Basix <basix@basix.tech>
2022-02-08 21:43:16 +0900

    Fix Apollo Devtools
    But why would you put script on there?

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,7 @@
 import { hydrateRoot } from "react-dom";
 import { RemixBrowser } from "remix";
 
+// Remove Apollo Devtool hooks.
+document.querySelectorAll("html>script[type=module]").forEach(v => v.remove());
+
 hydrateRoot(document, <RemixBrowser />);
