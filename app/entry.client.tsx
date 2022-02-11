35cb4e882422eaa0bc2a2266fc9628d275899cb3
Basix <basix@basix.tech>
2022-02-12 08:11:01 +0900

    Improve workaround

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -6,10 +6,14 @@ import { hydrateRoot } from "react-dom/client";
 HTMLTemplateElement.prototype.appendChild = function (child) {
   return this.content.appendChild(child);
 }
-
 HTMLTemplateElement.prototype.removeChild = function (child) {
   return this.content.removeChild(child);
 }
+Object.defineProperty(HTMLTemplateElement.prototype, "firstChild", {
+  get() {
+    return this.content.firstChild;
+  }
+})
 
 startTransition(() => {
   hydrateRoot(
